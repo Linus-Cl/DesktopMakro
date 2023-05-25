8eceed117989ae006ba2e2e82bc1c40cd98ccd6f
y0105877 <l.claussen@tu-braunschweig.de>
2023-05-25 15:17:17 +0200

    init

---
+++ DesktopMakro/DesktopMakro/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Program.cs
+ * Author: Linus-Cl
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopMakro
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
